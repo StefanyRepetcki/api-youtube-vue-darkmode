a415f509a78c48333f5bb96362bec2d4ad838eff
Stefany Repetcki <stefany.repetcki@contabilizei.com.br>
2022-01-26 16:28:18 -0300

    feat(views): Home

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="home">
+    <about />
+
+    <subscribe />
+
+    <social />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    components: {
+      About: () => import('@/components/home/About'),
+    },
+  }
+</script>
