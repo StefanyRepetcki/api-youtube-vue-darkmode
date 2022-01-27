17dff130767df0a63ddd7978c5beaa77e35e588d
StefanyRepetcki <StefanyRepetcki@users.noreply.github.com>
2022-01-27 22:54:07 +0000

    deploy: 13d88a3caca263491a75a76b09109730add495f0

--- src/components/home/about.service.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:3000/file/',
-    withCredentials: false,
-  });
-
-export const buscarYoutube = params => api.get(`/url/${params}`);
